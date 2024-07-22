46a7b186b9eec50331d3ab74626925ff937c603b
Victor <victor.alarcon@utp.edu.co>
2024-07-22 11:19:50 -0500

    Agregar archivos de proyecto.

---
+++ ModuloConsultasGiGas/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ModuloConsultasGiGas
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
